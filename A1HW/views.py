8c3a31815bc61e51473a2cc44885e565d3a20949
912-Dutescu-HoriaIonut <horia.dutescu@stud.ubbcluj.ro>
2023-03-30 11:39:47 +0300

    Swagger
    swagger

--- A1HW/views.py
+++ A1HW/views.py
@@ -13,7 +13,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import  GenericAPIView,ListAPIView
 from rest_framework.viewsets import ModelViewSet
-
+from drf_yasg.inspectors import SwaggerAutoSchema
 from rest_framework.views import APIView
 
 
