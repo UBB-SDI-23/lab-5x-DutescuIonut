ae5c8b11c60136bcf6f97b3cdbb3977c49ab0279
912-Dutescu-HoriaIonut <horia.dutescu@stud.ubbcluj.ro>
2023-03-31 16:15:37 +0300

    LH

--- Frontend/Frontend/src/main.tsx
+++ Frontend/Frontend/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-export const GlobalURL="https://ec2-16-16-97-142.eu-north-1.compute.amazonaws.com";//This may change in AWS;
-
+// export const GlobalURL="https://ec2-16-16-97-142.eu-north-1.compute.amazonaws.com";//This may change in AWS;
+	export const GlobalURL="http://127.0.0.1:8000";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
