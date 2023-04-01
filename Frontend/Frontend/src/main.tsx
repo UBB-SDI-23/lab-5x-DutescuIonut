1c4bf1aa5b8d10d3afacdcfb2c0d7d7f0b0476f6
912-Dutescu-HoriaIonut <horia.dutescu@stud.ubbcluj.ro>
2023-04-01 12:00:56 +0300

    as
    as

--- Frontend/Frontend/src/main.tsx
+++ Frontend/Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-export const GlobalURL="https://ec2-16-16-100-78.eu-north-1.compute.amazonaws.com";//This may change in AWS;
+export const GlobalURL="https://ec2-13-50-15-234.eu-north-1.compute.amazonaws.com";//This may change in AWS;
 	// export const GlobalURL="http://127.0.0.1:8000";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
